5e6156588661f5bedfcc4652b3a49949cb5e0541
janko <janko.ulaga@gmail.com>
2016-07-15 16:54:56 +0200

    Added PlayerActor tests.

--- src/test/scala/com/ju/player/PlayerActorSpec.scala
+++ src/test/scala/com/ju/player/PlayerActorSpec.scala
@@ -1,17 +1,33 @@
 package com.ju.player
 
 import akka.actor.ActorSystem
-import akka.testkit.{ImplicitSender, TestKit}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import com.ju.player.PlayerActor.JoinGame
+import com.ju.rulebook.Rock
 import org.scalatest.{Matchers, WordSpecLike}
 
 class PlayerActorSpec extends TestKit(ActorSystem("PlayerActorSpec")) with ImplicitSender with WordSpecLike with Matchers {
   "PlayerActor" must {
     "Once created, send a message to the Game that it's joining it" in {
-
+      val game = new TestProbe(system)
+      val player = system.actorOf(PlayerActor.props(game.ref))
+      game.expectMsg(JoinGame)
     }
 
     "Accept a move from a sender" in {
+      val game = new TestProbe(system)
+      val player = system.actorOf(PlayerActor.props(game.ref))
+      game.expectMsg(JoinGame)
+      player ! Rock
+      game.expectMsg(Rock)
+    }
 
+    "Ignore silly messages" in {
+      val game = new TestProbe(system)
+      val player = system.actorOf(PlayerActor.props(game.ref))
+      game.expectMsg(JoinGame)
+      player ! "SheepInWolvesClothing"
+      game.expectNoMsg()
     }
   }
 }
