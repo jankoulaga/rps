d8df580a16a0d7f80c1b9d79eb128ad8f40c88c3
janko <janko.ulaga@gmail.com>
2016-07-15 15:09:59 +0200

    Implemented tests for Move checks.

--- src/test/scala/com/ju/rulebook/MoveSpec.scala
+++ src/test/scala/com/ju/rulebook/MoveSpec.scala
@@ -5,36 +5,38 @@ import org.scalatest.{Matchers, WordSpecLike}
 class MoveSpec extends WordSpecLike with Matchers {
   "Rock" must {
     "Beat Scissors" in {
-
+      Rock.matchAgainst(Scissors) shouldEqual Win
     }
     "Draw with Rock" in {
-
+      Rock.matchAgainst(Rock) shouldEqual Draw
     }
-    "Lose against Paper"
+    "Lose against Paper" in {
+      Rock.matchAgainst(Paper) shouldEqual Loss
+    }
   }
 
   "Scissors" must {
     "Lose against Rock" in {
-
+      Scissors.matchAgainst(Rock) shouldEqual Loss
     }
     "Draw with Scissors" in {
-
+      Scissors.matchAgainst(Scissors) shouldEqual Draw
     }
     "Beat Paper" in {
-
+      Scissors.matchAgainst(Paper) shouldEqual Win
     }
   }
 
 
   "Paper" must {
     "Lose against Scissors" in {
-
+      Paper.matchAgainst(Scissors) shouldEqual Loss
     }
     "Beat Rock" in {
-
+      Paper.matchAgainst(Rock) shouldEqual Win
     }
     "Draw with Paper" in {
-
+      Paper.matchAgainst(Paper) shouldEqual Draw
     }
   }
 
